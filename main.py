ea9fc8325214f0637430b5ee518124cb20b25780
Abin Thomas <followgamer102@gmail.com>
2025-01-14 11:08:07 +0530

    feat: link scraper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
 from tabulate import tabulate
+
 def srape(url,element):
   try:
     headers = {
@@ -26,6 +27,7 @@ def srape(url,element):
             print("\n")
   except Exception as e:
     print("Error in scraping the data")
+
 def scrape(url,_class,_id):
     try:
         headers = {
@@ -48,12 +50,23 @@ def scrape(url,_class,_id):
                  print(f"{_id}: {attribute_value}")
     except Exception as e:
         print("Error in fetching the data",e)
-        
-        
-        
+            
+def scrape_links(url):
+    try:
+         headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"}
+         res = req.get(url, headers=headers, timeout=600)
+         res.raise_for_status() 
+         data = res.text
+         soup = bs(data, 'html.parser')
+         d=soup.find_all('a')
+         for i in d:
+            print(i.get('href'))
+    except Exception as e:
+        print("Error in fetching the data",e)
         
 url = input("Enter the URL: ")
-choice = input("\n1. Scrape with class name.\n2. Scrape with element.\n3.Scarpe with id.\n4.Scrape links\nChoose an option:")
+choice = input("\n1. Scrape with class name.\n2. Scrape with element.\n3. Scarpe with id.\n4. Scrape hidden links\nChoose an option:")
 if choice == '1':
     _class = input("Enter the class you want to scrape: ")
     _id = input("Enter the attribute you want to scrape else press 0: ")
@@ -61,6 +74,7 @@ if choice == '1':
 elif choice == '2':
     element = input("Enter the element you want to scrape: ")
     srape(url,element)
-else:
+elif choice == '3':
     id = input("Enter the element you want to scrape: ")
-
+elif choice == '4':
+    scrape_links(url)
