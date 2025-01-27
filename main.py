b9544433dee7bda8b5aa1719645dfe59b7ba8256
Abin Thomas <followgamer102@gmail.com>
2025-01-28 01:08:17 +0530

    fix: build issues

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from tabulate import tabulate
 import os
 from colorama import Fore
 import pyfiglet 
-
 def srape(url,element):
   try:
     headers = {
@@ -22,16 +21,15 @@ def srape(url,element):
             print("\n")
     else:
         d=soup.find_all(element)
-        #print(d)
         if len(d)==0:
             print("No such element found")
         else:
-            print(d)
-            print("\n")
+            for i in d:
+                print(i.get_text(strip=True))
   except Exception as e:
     print(Fore.RED +"Error in scraping the data")
 
-def scrape(url,_class,_id):
+def scrape(url,_class):
     try:
         headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"}
@@ -39,18 +37,12 @@ def scrape(url,_class,_id):
         res.raise_for_status() 
         data = res.text
         soup = bs(data, 'html.parser')
-       # print(soup.prettify())
-
         d = soup.find_all(class_=_class)
-        #print(d)
         if len(d)==0:
             print("No such class found")
         else:
             for i in d:
                 print(i.get_text(strip=True))
-                if _id:
-                 attribute_value = i.get(_id, "Attribute not found")
-                 print(f"{_id}: {attribute_value}")
     except Exception as e:
         print(Fore.RED +"Error in fetching the data",e)
             
@@ -65,7 +57,6 @@ def scrape_links(url):
          d=soup.find_all('a')
          print(d)
          for i in d:
-            print(i)
             print(i.get('href'))
     except Exception as e:
         print(Fore.RED +"Error in fetching the data",e)
@@ -95,9 +86,28 @@ def dork(url):
             print(i[2])
          print(Fore.YELLOW +"The links are saved in Results/links.txt"+Fore.WHITE)
         else:
-            print("No data found")
+            print(Fore.RED+"No data found")
     except Exception as e:
         print(e)
+def scrapeId(url,_id):
+    try:
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"}
+        res = req.get(url, headers=headers, timeout=600)
+        res.raise_for_status() 
+        data = res.text
+        soup = bs(data, 'html.parser')
+        d = soup.find_all(id=_id)
+        if len(d)==0:
+            print("No such id found")
+        else:
+            for i in d:
+                result = i.get_text(strip=True, separator=' | ')
+                result = result.replace("|","\n")
+                print(Fore.WHITE+result)
+    except Exception as e:
+        print(Fore.RED +"Error in fetching the data",e)
+            
 styled_text=pyfiglet.figlet_format('WEB SCRAPER',font= 'doom')
 print(Fore.BLUE + styled_text)
 print(Fore.LIGHTBLUE_EX + "https://github.com/abin-karukappallil/Advanced-web-scraper\n"+Fore.WHITE)
@@ -105,13 +115,13 @@ url = input("Enter the URL: ")
 choice = input(Fore.BLUE + "\n1. Scrape with class name.\n2. Scrape with element.\n3. Scarpe with id.\n4. Scrape hidden links\n5. Scrape confidential documents\n Choose an option:")
 if choice == '1':
     _class = input("Enter the class you want to scrape: ")
-    _id = input("Enter the attribute you want to scrape else press 0: ")
-    scrape(url,_class,_id)
+    scrape(url,_class)
 elif choice == '2':
     element = input("Enter the element you want to scrape: ")
     srape(url,element)
 elif choice == '3':
     id = input("Enter the element you want to scrape: ")
+    scrapeId(url,id)
 elif choice == '4':
     scrape_links(url)
 elif choice == '5':
